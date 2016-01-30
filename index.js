7d88185ec824a2441e717ccefe24a1acb3d8a3d9
schnie <greg@differential.com>
2016-01-30 02:43:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * Main entry point when imported.
+ * Export important modules.
+ */
+
+export { default as swf } from './lib/swf/client';
+export { default as activity } from './lib/swf/activity';
+export { default as decider } from './lib/swf/decider';
+export { default as activityConfigProvider } from './lib/swf/activityConfigProvider';
+export { default as workflowStateProvider } from './lib/swf/workflowStateProvider';
+export { default as registerActivity } from './lib/swf/registerActivity';
