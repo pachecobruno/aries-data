53ba9865e98e963f9bc3b0a69a02133da3a92c68
schnie <greg@differential.com>
2016-02-02 11:33:16 -0500

    boot stuff

--- index.js
+++ index.js
@@ -3,9 +3,19 @@
  * Export important modules.
  */
 
+import stampit from 'stampit';
+import logger from './lib/util/logger';
+
+// Default export is just a basic stamp.
+export default stampit.init(function() {
+    // Set up a default logger.
+    this.log = logger('mixin');
+});
+
 export { default as swf } from './lib/swf/client';
 export { default as activity } from './lib/swf/activity';
 export { default as decider } from './lib/swf/decider';
 export { default as activityConfigProvider } from './lib/swf/activityConfigProvider';
 export { default as workflowStateProvider } from './lib/swf/workflowStateProvider';
 export { default as registerActivity } from './lib/swf/registerActivity';
+export { default as boot } from './lib/boot/boot.js';
