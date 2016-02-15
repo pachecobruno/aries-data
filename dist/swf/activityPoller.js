fe4e57d8b146690c03e4fcba4f372cdd97ecb0d3
schnie <greg@differential.com>
2016-02-15 10:20:25 -0500

    allow arrays to be returned from config providers

--- dist/swf/activityPoller.js
+++ dist/swf/activityPoller.js
@@ -12,6 +12,10 @@ var _lodash = require('lodash.isstring');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
+var _lodash3 = require('lodash.flatten');
+
+var _lodash4 = _interopRequireDefault(_lodash3);
+
 var _poller = require('./poller');
 
 var _poller2 = _interopRequireDefault(_poller);
@@ -45,7 +49,7 @@ exports.default = _stampit2.default.compose(_poller2.default, _stampit2.default.
         var _this = this;
 
         return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
-            var activityType, module, task, config, output;
+            var activityType, module, task, config, args, output;
             return regeneratorRuntime.wrap(function _callee$(_context) {
                 while (1) {
                     switch (_context.prev = _context.next) {
@@ -69,32 +73,45 @@ exports.default = _stampit2.default.compose(_poller2.default, _stampit2.default.
                             // Create an activityTask.
                             task = (0, _activityTask2.default)(result);
 
-                            // If this module has a configProvider, get it.  Always produce an object.
-
-                            _context.t0 = Object;
-                            _context.t1 = {};
+                            // If this module has a configProvider, run it.
+                            // This allows configProviders to return a single object, or array.
+                            // This array will then be applied to the onTask function,
+                            // resulting in ability to pass multiple params to onTask.
+                            // By convention, the first parameter passed in will always be the activityTask
+                            // and the second should always be the task configuration.
+                            // This just allows more flexibility for config providers to provide
+                            // additional contextual information.
 
                             if (!module.getConfig) {
-                                _context.next = 14;
+                                _context.next = 13;
                                 break;
                             }
 
-                            _context.next = 11;
+                            _context.next = 9;
                             return module.getConfig(task);
 
-                        case 11:
-                            _context.t2 = _context.sent;
-                            _context.next = 15;
+                        case 9:
+                            _context.t1 = _context.sent;
+                            _context.t0 = [_context.t1];
+                            _context.next = 14;
                             break;
 
-                        case 14:
-                            _context.t2 = {};
+                        case 13:
+                            _context.t0 = [{}];
 
-                        case 15:
-                            _context.t3 = _context.t2;
-                            config = _context.t0.assign.call(_context.t0, _context.t1, _context.t3);
+                        case 14:
+                            config = _context.t0;
+
+                            // Ensure a single dimension array.
+                            args = (0, _lodash4.default)(config);
+
+                            // Add task as first arg.
+
+                            args.unshift(task);
+
+                            // Run the task.
                             _context.next = 19;
-                            return module.onTask(task, config);
+                            return module.onTask.apply(module, args);
 
                         case 19:
                             output = _context.sent;
@@ -113,7 +130,7 @@ exports.default = _stampit2.default.compose(_poller2.default, _stampit2.default.
                             }
 
                             _context.next = 25;
-                            return module.afterTask(task, config);
+                            return module.afterTask.apply(module, args);
 
                         case 25:
                             _context.next = 27;
@@ -128,9 +145,9 @@ exports.default = _stampit2.default.compose(_poller2.default, _stampit2.default.
 
                         case 29:
                             _context.prev = 29;
-                            _context.t4 = _context['catch'](0);
+                            _context.t2 = _context['catch'](0);
 
-                            log(_context.t4);
+                            log(_context.t2);
                             // Respond failure.
                             _context.next = 34;
                             return _this.client.respondActivityTaskFailed({
