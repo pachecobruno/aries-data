4dbe2a278c3f9100bdc27b91838b360cd45106a9
Mike Perry <mike@astronomer.io>
2017-02-06 14:48:09 -0500

    config.connection and config.connection.vpnConnection will now be decrypted if _encrypted = true. The encryptionKey must be provided via env. connection.code and connection.vpnConnection.code are not decrypted.

--- test/test.js
+++ test/test.js
@@ -2,124 +2,236 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 import nock from 'nock';
+import cryptobject from 'cryptobject';
 import * as tunnel from '../lib/util/tunnel';
-import execute, { parse } from '../lib/cli/execute';
+import execute, { parse, decryptConfig } from '../lib/cli/execute';
 
 chai.use(chaiAsPromised);
 const assert = chai.assert;
 
 describe('execute', function() {
 
-  describe('#execute', function() {
-    it('executes goodActivity', async function() {
-      // String params, from airflow.
-      const task = '{}';
-      const config = '{}';
-      const executionDate = '2016-08-17T19:30:00';
+    describe('#execute', function() {
+        it('executes goodActivity', async function() {
+            // String params, from airflow.
+            const task = '{}';
+            const config = '{}';
+            const executionDate = '2016-08-17T19:30:00';
 
-      // Absolute path to mock activity.
-      const repo = `${process.cwd()}/test/goodActivity`;
+            // Absolute path to mock activity.
+            const repo = `${process.cwd()}/test/goodActivity`;
 
-      // Construct param object.
-      const params = { repo, _: [task, config, executionDate] };
+            // Construct param object.
+            const params = { repo, _: [task, config, executionDate] };
 
-      // Execute!
-      const result = await execute(params);
+            // Execute!
+            const result = await execute(params);
 
-      // Make sure we have input and the result from the mock task.
-      assert.equal(result.input, 'result');
+            // Make sure we have input and the result from the mock task.
+            assert.equal(result.input, 'result');
+        });
+
+        it('it rejects badActivity', function() {
+            // String params, from airflow.
+            const task = '{}';
+            const config = '{}';
+            const executionDate = '2016-08-17T19:30:00';
+
+            // Absolute path to mock activity.
+            const repo = `${process.cwd()}/test/badActivity`;
+
+            // Construct param object.
+            const params = { repo, _: [task, config, executionDate] };
+
+            // Assert that the promise should fail.
+            return assert.isRejected(execute(params));
+        });
+
+        it('calls openTunnel if vpnConnection exists on config.connection', async function() {
+            // String params, from airflow.
+            const task = '{}';
+            const config = {
+                connection: {
+                    vpnConnection: {
+                        type: 'forticlientsslvpn',
+                    },
+                },
+            };
+            const executionDate = '2016-08-17T19:30:00';
+
+            // Absolute path to mock activity.
+            const repo = `${process.cwd()}/test/goodActivity`;
+
+            // Construct param object.
+            const params = { repo, _: [task, JSON.stringify(config), executionDate] };
+
+            // stub openTunnel
+            const stub = sinon.stub(tunnel, 'createTunnel', () => {});
+
+            // Execute!
+            const result = await execute(params);
+            assert(stub.calledOnce, 'createTunnel should be called if a connection has a vpnConnection param');
+        });
     });
 
-    it('it rejects badActivity', function() {
-      // String params, from airflow.
-      const task = '{}';
-      const config = '{}';
-      const executionDate = '2016-08-17T19:30:00';
+    describe('#decryptConfig', function () {
+        it('returns the original config if no encryptionKey', function () {
+            const connection = {
+                host: 'example.com',
+                port: 1234,
+                user: 'user',
+                pass: 'pass',
+                _encrypted: true,
+            };
+            const encryptionKey = 'test-key';
+            const { encryptor } = cryptobject;
+            const encryptedConnection = encryptor(encryptionKey)(connection);
+            const config = decryptConfig({ connection: encryptedConnection });
+            assert.deepEqual(config.connection, encryptedConnection);
+        });
 
-      // Absolute path to mock activity.
-      const repo = `${process.cwd()}/test/badActivity`;
+        it('decrypts config.connection', function () {
+            const connection = {
+                host: 'example.com',
+                port: 1234,
+                user: 'user',
+                pass: 'pass',
+                _encrypted: true,
+            };
+            const encryptionKey = 'test-key';
+            const { encryptor } = cryptobject;
+            const encryptedConnection = encryptor(encryptionKey)(connection);
+            const config = decryptConfig({ connection: encryptedConnection }, encryptionKey);
+            assert.deepEqual(config.connection, connection);
+        });
 
-      // Construct param object.
-      const params = { repo, _: [task, config, executionDate] };
+        it('does not decrypt config.connection if _encrypted is not true', function () {
+            const connection = {
+                host: 'example.com',
+                port: 1234,
+                user: 'user',
+                pass: 'pass',
+                _encrypted: false,
+            };
+            const encryptionKey = 'test-key';
+            const config = decryptConfig({ connection }, encryptionKey);
+            assert.deepEqual(config.connection, connection);
+        });
 
-      // Assert that the promise should fail.
-      return assert.isRejected(execute(params));
+        it('decrypts connection but not vpnConnection when vpnConnection._encrypted = false', function () {
+            const connection = {
+                host: 'example.com',
+                port: 1234,
+                user: 'user',
+                pass: 'pass',
+                _encrypted: true,
+            };
+            const vpnConnection = {
+                host: 'example.com',
+                user: 'user',
+                port: 1234,
+                _encrypted: false,
+            };
+
+            const encryptionKey = 'test-key';
+            const { encryptor } = cryptobject;
+            const encryptedConnection = encryptor(encryptionKey)(connection);
+            encryptedConnection.vpnConnection = vpnConnection;
+            const config = decryptConfig({ connection: encryptedConnection }, encryptionKey);
+            assert.deepEqual(config.connection, { ...connection, vpnConnection });
+        });
+
+        it('decrypts vpnConnection but not connection when connection._encrypted = false', function () {
+            const connection = {
+                host: 'example.com',
+                port: 1234,
+                user: 'user',
+                pass: 'pass',
+                _encrypted: false,
+            };
+            const vpnConnection = {
+                host: 'example.com',
+                user: 'user',
+                port: 1234,
+                _encrypted: true,
+            };
+
+            const encryptionKey = 'test-key';
+            const { encryptor } = cryptobject;
+            const encryptedVpnConnection = encryptor(encryptionKey)(vpnConnection);
+            const config = decryptConfig({
+                connection: { ...connection, vpnConnection: encryptedVpnConnection },
+            }, encryptionKey);
+            assert.deepEqual(config.connection, { ...connection, vpnConnection });
+        });
+
+        it('does not decrypt connection.code', function () {
+            const connection = {
+                code: 'code',
+                host: 'example.com',
+                vpnConnection: {
+                    code: 'code',
+                    host: 'example.com',
+                    _encrypted: true,
+                },
+                _encrypted: true,
+            };
+
+            const encryptionKey = 'test-key';
+            const config = decryptConfig({ connection }, encryptionKey);
+            console.log(config);
+            assert(config.connection.code === 'code', 'decryptConfig should not decrypt code');
+            assert(config.connection.vpnConnection.code === 'code', 'decryptConfig should not decrypt code');
+        });
     });
 
-    it('calls openTunnel if vpnConnection exists on config.connection', async function() {
-      // String params, from airflow.
-      const task = '{}';
-      const config = {
-        connection: {
-          vpnConnection:{
-            type: 'forticlientsslvpn'
-          }
-        }
-      };
-      const executionDate = '2016-08-17T19:30:00';
+    describe('#parse', function () {
+        it('parses arguments', function () {
+            const task = '{ "key": "val" }';
+            const config = '{}';
+            const executionDate = '2016-08-17T19:30:00';
+            const args = [task, config, executionDate];
 
-      // Absolute path to mock activity.
-      const repo = `${process.cwd()}/test/goodActivity`;
+            const [parsedTask, parsedConfig, parsedExecutionDate] = parse(args);
 
-      // Construct param object.
-      const params = { repo, _: [task, JSON.stringify(config), executionDate] };
+            assert.deepEqual(parsedTask, { key: 'val' });
+            assert.deepEqual(parsedConfig, {});
+            assert.deepEqual(parsedExecutionDate, new Date(executionDate));
+        });
 
-      //stub openTunnel
-      const stub = sinon.stub(tunnel, 'createTunnel', () => {});
+        it('parses a non json object', function () {
+            const task = 'None';
 
-      // Execute!
-      const result = await execute(params);
-      assert(stub.calledOnce, "createTunnel should be called if a connection has a vpnConnection param");
-    });
-  });
+            const [parsedTask] = parse([task]);
 
-  describe('#parse', function() {
-    it('parses arguments', function() {
-      const task = '{ "key": "val" }';
-      const config = '{}';
-      const executionDate = '2016-08-17T19:30:00';
-      const args = [task, config, executionDate];
-
-      const [parsedTask, parsedConfig, parsedExecutionDate ] = parse(args);
-
-      assert.deepEqual(parsedTask, { key: "val" });
-      assert.deepEqual(parsedConfig, {});
-      assert.deepEqual(parsedExecutionDate, new Date(executionDate));
+            assert.equal(parsedTask, task);
+        });
     });
 
-    it('parses a non json object', function() {
-      const task = 'None';
+    describe('singleS3StreamInput', function() {
+        it.skip('exits early with no data', async function() {
+            // String params, from airflow.
+            const task = '{"input":{"key":"someobject"}}';
+            const config = '{}';
+            const executionDate = '2016-08-17T19:30:00';
 
-      const [ parsedTask ] = parse([task]);
+            // // Mock up env var.
+            process.env.AWS_S3_TEMP_BUCKET = 'astronomer-workflows';
+            process.env.AWS_REGION = 'us-east-1';
 
-      assert.equal(parsedTask, task);
+            // Absolute path to mock activity.
+            const repo = `${process.cwd()}/test/inputStreamActivity`;
+
+            // Construct param object.
+            const params = { repo, _: [task, config, executionDate] };
+
+            nock(`https://${process.env.AWS_S3_TEMP_BUCKET}.s3.amazonaws.com`);
+
+                // Execute!
+                const result = await execute(params);
+
+            // Make sure we have input and the result from the mock task.
+            assert.equal(result.input, 'result');
+        });
     });
-  });
-
-  describe('singleS3StreamInput', function() {
-    it.skip('exits early with no data', async function() {
-      // String params, from airflow.
-      const task = '{"input":{"key":"someobject"}}';
-      const config = '{}';
-      const executionDate = '2016-08-17T19:30:00';
-
-      // // Mock up env var.
-      process.env.AWS_S3_TEMP_BUCKET = 'astronomer-workflows';
-      process.env.AWS_REGION = 'us-east-1';
-
-      // Absolute path to mock activity.
-      const repo = `${process.cwd()}/test/inputStreamActivity`;
-
-      // Construct param object.
-      const params = { repo, _: [task, config, executionDate] };
-
-      nock(`https://${process.env.AWS_S3_TEMP_BUCKET}.s3.amazonaws.com`);
-
-      // Execute!
-      const result = await execute(params);
-
-      // Make sure we have input and the result from the mock task.
-      assert.equal(result.input, 'result');
-    });
-  });
 });
