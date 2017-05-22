243d3886cdbc5545eea9c39621aebb66b36adb72
Mike Perry <mike@astronomer.io>
2017-05-22 03:55:51 -0400

    built with babel

--- dist/cli/execute.js
+++ dist/cli/execute.js
@@ -5,6 +5,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.runTask = undefined;
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 /**
@@ -61,11 +63,20 @@ var runTask = exports.runTask = function () {
 
 
 exports.JSONparse = JSONparse;
+exports.decryptConfig = decryptConfig;
 exports.parse = parse;
 
 var _moment = require('moment');
 
 var _moment2 = _interopRequireDefault(_moment);
+
+var _lodash = require('lodash.omit');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _cryptobject = require('cryptobject');
+
+var _cryptobject2 = _interopRequireDefault(_cryptobject);
 
 var _logger = require('../util/logger');
 
@@ -79,7 +90,9 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
+var decryptor = _cryptobject2.default.decryptor;
 // Create logger.
+
 var log = (0, _logger2.default)(__filename);function JSONparse(str) {
     try {
         return JSON.parse(str);
@@ -89,19 +102,62 @@ var log = (0, _logger2.default)(__filename);function JSONparse(str) {
 }
 
 /**
+ * decrypts a connection if it's defined and
+ * connection._encrypted is true
+ * @return {object} the decrypted connection if _encrypted is true.
+ * Otherwise the original connection
+ * Note: connection.code is not decrypted
+ */
+function decryptConnection(connection, encryptionKey) {
+    if (connection && connection._encrypted) {
+        // don't decrypt object.code
+        var ignore = ['code'];
+        return decryptor(encryptionKey)(connection, ignore);
+    }
+    return connection;
+}
+
+/**
+ * checks for a connection object on config and decrypts it
+ * if ENCRYPTION_KEY was passed into the environment and
+ * connection._encrypted is true. If connection.vpnConnection
+ * exists, it will also be decrypted, also only if vpnConnection._encrypted
+ * is true..
+ * @return {Object} the config with the connection decrypted
+ */
+function decryptConfig(config) {
+    var encryptionKey = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : process.env.ENCRYPTION_KEY;
+
+    // if no encryptionKey or connection, just return the config as is
+    if (!encryptionKey || !config.connection) return config;
+    // exclude the vpnConnection so we can check if it's encrypted separately
+    var vpnConnectionOmitted = (0, _lodash2.default)(config.connection, ['vpnConnection']);
+    // decrypt connection if _encrypted = true
+    var connection = decryptConnection(vpnConnectionOmitted, encryptionKey);
+    // decrypt vpnConnection if _encrypted = true
+    var vpnConnection = decryptConnection(config.connection.vpnConnection, encryptionKey);
+    // put the config back together
+    if (vpnConnection) {
+        connection.vpnConnection = vpnConnection;
+    }
+    return _extends({}, config, { connection: connection });
+}
+
+/**
  * Parse strings from cli.
  */
 function parse(args) {
     // Destructure.
-    var _args2 = _slicedToArray(args, 3),
+    var _args2 = _slicedToArray(args, 4),
         task = _args2[0],
         config = _args2[1],
-        executionDate = _args2[2];
+        executionDate = _args2[2],
+        nextExecutionDate = _args2[3];
 
     // Return the parsed version.
 
 
-    return [JSONparse(task), JSONparse(config), new Date(executionDate)];
+    return [JSONparse(task), decryptConfig(JSONparse(config)), new Date(executionDate), nextExecutionDate ? new Date(nextExecutionDate) : null];
 }
 
 /**
@@ -188,7 +244,7 @@ exports.default = function () {
         }, _callee2, this, [[0, 17, 21, 24]]);
     }));
 
-    function execute(_x3) {
+    function execute(_x4) {
         return _ref2.apply(this, arguments);
     }
 
